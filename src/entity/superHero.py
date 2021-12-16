1bd5d7294323026aec5da97da3db72081e223abc
ricardosorli <ricardosorli@gmail.com>
2021-12-16 13:10:03 +0100

    First version

--- src/entity/superHero.py
+++ src/entity/superHero.py
@@ -1,11 +1,15 @@
 
 class superHero:
+    def __init__ (self,name,power,secretName,city,location):
+       self.name = name
+       self.power = power
+       self.secretName = secretName
+       self.city = city
+       self.location = location
 
-    """
-    Any simple class should be defined here. You must code some business logic, which can be
-    executed on its instance.
-    """
-  
+
+    def maxPower (self,increase):
+       self.power = increase
 
 
     
