80ef05c1af4b3478c5999be656beef5efb48d266
Danrodsf <Danielrodriguezserafin@gmail.com>
2021-12-16 13:02:59 +0100

    Segundo Intento

--- src/entity/superHero.py
+++ src/entity/superHero.py
@@ -2,11 +2,14 @@
 class superHero:
 
     def __init__(self, name, power, secretName, city, location):
-        self.name: name
-        self.power: power
-        self.secretName: secretName
-        self.city: city
-        self.location: location
+        self.name = name
+        self.power = power
+        self.secretName = secretName
+        self.city = city
+        self.location = location
+
+    def maxPower(self, power2):
+        return self.power + power2
 
 
     
