1d82e5ffa14d95730e0f790bbbd12f7f67e25a77
NeonFixr <yagosan89@gmail.com>
2021-12-16 13:33:18 +0100

    Finished

--- test/TestSuperHero.py
+++ test/TestSuperHero.py
@@ -4,7 +4,7 @@ from entity.superHero import superHero
 class TestSuperHero(unittest.TestCase):
 
     def setUp(self):
-        self.s1 = superHero('Robin', 1024, 'Dick Grayson', 'Gotham', 'Batcave')
+        self.s1 = superHero('Robin', 1024, 'Dick Grayson', 'Gotham', 'Batcave', 8000)
 
     def test_existClass(self):
         self.assertTrue(self.s1)
@@ -32,9 +32,9 @@ class TestSuperHero(unittest.TestCase):
         self.assertGreaterEqual(self.s1.power, 1024)
 
     def test_instance(self):
-        s2 = superHero('Robin II', 2500, 'Jason Todd', 'Gotham', 'BatCave')
-        self.assertIsInstance(s2, superHero, 'Object is no instance of Superhero')
-        self.assertNotEqual(id(self.s1), id(s2), 'Variables contain different superheroes')
+        s2 = superHero('Robin II', 2500, 'Jason Todd', 'Gotham', 'BatCave', 8000)
+        self.assertIsInstance(s2, superHero, 'Object is no instance of Superhero',)
+        self.assertNotEqual(id(self.s1), id(s2), 'Variables contain different superheroes',)
 
 
 if __name__ == '__main__':
