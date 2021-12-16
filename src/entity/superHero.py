128004a5773a57331c2550ac4dbf4846007ed3e8
Fabrizio Pironi <fabriziopironi@gmail.com>
2021-12-16 12:44:22 +0100

    init y maxPower listo

--- src/entity/superHero.py
+++ src/entity/superHero.py
@@ -5,7 +5,12 @@ class superHero:
     Any simple class should be defined here. You must code some business logic, which can be
     executed on its instance.
     """
-  
+    def __init__(self, name, power, secretName, city, location):
+        self.name = name
+        self.power = power
+        self.realName = secretName
+        self.city = city
+        self.base = location
 
-
-    
+    def maxPower(sel0, power):
+        power = power + 200
