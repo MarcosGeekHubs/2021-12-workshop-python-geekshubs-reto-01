61b730d8d039f11f30ba269622e3c5e8af6501a1
lug <lug@rrcs-172-254-6-16.nyc.biz.rr.com>
2021-12-16 13:26:05 +0100

    superHero class made as the client has requested

--- src/entity/superHero.py
+++ src/entity/superHero.py
@@ -5,7 +5,15 @@ class superHero:
     Any simple class should be defined here. You must code some business logic, which can be
     executed on its instance.
     """
-  
-
-
-    
+    def __init__(self, name, power, secretName, city, location):
+        self.name = name
+        if power < 1024:
+            raise ValueError("The power need to be more than 1024.")
+        else:
+            self.power = power
+        self.secretName = secretName
+        self.city = city
+        self.location = location
+        
+    def maxPower(self, power):
+        self.power += power
